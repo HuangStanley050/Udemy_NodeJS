decf9a67f35c95fce5fc9d979f563a778274bc46
Stanley Huang <infamous_godhand@yahoo.com>
2018-11-02 04:18:24 +0000

    split up ejs to header/end/navigation

--- routes/admin.js
+++ routes/admin.js
@@ -7,7 +7,14 @@ const products = [];
 router.get('/add-product', (req, res, next) => {
     //console.log("middleware1");
     //res.sendFile(path.join(__dirname, '../views', 'add-product.html'));
-    res.render('add-product', { pageTitle: "Add Product" });
+    res.render('add-product', {
+        path: '/admin/add-product',
+        pageTitle: "Add Product",
+        formsCSS: true,
+        productCSS: true,
+        activeAddProduct: true
+
+    });
 });
 
 router.post('/add-product', (req, res) => {
